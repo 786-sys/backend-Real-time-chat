4eee8ca7042688f8ba9269e7f6ca9f13d5065b7c
Mohammed Ali <mohammed.sayed16677@sakec.ac.in>
2026-07-17 00:53:23 +0530

    u

--- src/db/index.js
+++ src/db/index.js
@@ -9,6 +9,10 @@ export const ConnectDB = async () => {
     try {
         const connection = await mongoose.connect(process.env.MONGODB_URL);
         console.log(`\n MongoDB connected !! DB HOST: ${connection.connection.host}`);
+        mongoose.connection.once("open", () => {
+            console.log("Host:", mongoose.connection.host);
+            console.log("Database:", mongoose.connection.name);
+        });
     } catch (error) {
         console.log("DB CONNECTION FAILED", error);
         process.exit(1);
